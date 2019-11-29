ed56f45f41a4acc766649dff606779e7cd0c3481
Alexander Glück <github@alexanderglueck.at>
2019-11-29 20:40:43 +0100

    Switch LoginActivity from volley to Retrofit2

--- app/src/main/java/com/alexanderglueck/urlpusher/LoginActivity.java
+++ app/src/main/java/com/alexanderglueck/urlpusher/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.alexanderglueck.urlpusher.responses.RemoveTokenResponse;
@@ -16,14 +17,10 @@ import com.alexanderglueck.urlpusher.responses.SessionResponse;
 
 import retrofit2.Call;
 import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
-    private static final String KEY_STATUS = "status";
     private static final String TAG = "LoginActivity";
-    private static final String KEY_MESSAGE = "message";
-    private static final String KEY_FULL_NAME = "full_name";
-    private static final String KEY_USERNAME = "email";
-    private static final String KEY_PASSWORD = "password";
     private static final String KEY_EMPTY = "";
     private EditText etUsername;
     private EditText etPassword;
@@ -119,74 +116,78 @@ public class LoginActivity extends AppCompatActivity {
         Call<SessionResponse> call = apiService.login(username, password);
         call.enqueue(new Callback<SessionResponse>() {
             @Override
-            public void onResponse(Call<SessionResponse> call, retrofit2.Response<SessionResponse> response) {
+            public void onResponse(@NonNull Call<SessionResponse> call, @NonNull Response<SessionResponse> response) {
                 sessionResponse = response.body();
-                Log.d("TAG", "Response = " + sessionResponse.getName() + sessionResponse.getId() + sessionResponse.getApiToken());
+                if (sessionResponse == null) {
+                    pDialog.dismiss();
+                } else {
+                    Log.d("TAG", "Response = " + sessionResponse.getName() + sessionResponse.getId() + sessionResponse.getApiToken());
 
-                boolean openDeviceSelection = false;
-                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCES_FILE, MODE_PRIVATE);
+                    boolean openDeviceSelection = false;
+                    SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(Constants.SHARED_PREFERENCES_FILE, MODE_PRIVATE);
 
-                if (sessionResponse.getId() != sharedPreferences.getInt(Constants.LAST_SIGNED_IN_USER_ID, 0)) {
-                    Log.d(TAG, "anderer user");
+                    if (sessionResponse.getId() != sharedPreferences.getInt(Constants.LAST_SIGNED_IN_USER_ID, 0)) {
+                        Log.d(TAG, "anderer user");
 
-                    // anderer user hat sich eingeloggt als zuvor
+                        // anderer user hat sich eingeloggt als zuvor
 
-                    // die jetzt eingeloggte id als zuletzte wegspeichern
-                    sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_USER_ID, sessionResponse.getId()).commit();
+                        // die jetzt eingeloggte id als zuletzte wegspeichern
+                        sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_USER_ID, sessionResponse.getId()).commit();
 
-                    // reset last used device id, because new user
-                    sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_DEVICE_ID, 0).commit();
+                        // reset last used device id, because new user
+                        sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_DEVICE_ID, 0).commit();
 
-                    // weil ein anderer user eingeloggt, alten token ungültig machen, sonst erhält er pushes vom anderen
-                    String token = sharedPreferences.getString(Constants.FCM_TOKEN, "");
-                    if (!token.equals("")) {
-                        deleteToken(token);
+                        // weil ein anderer user eingeloggt, alten token ungültig machen, sonst erhält er pushes vom anderen
+                        String token = sharedPreferences.getString(Constants.FCM_TOKEN, "");
+                        if (!token.equals("")) {
+                            deleteToken(token);
+                        }
+
+
+                        // weil neuer user auch device selection öffnen
+                        // last used device id leeren to not reconnect with wrong
+                        sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_DEVICE_ID, 0).commit();
+
+                        openDeviceSelection = true;
+
+                    } else {
+                        Log.d(TAG, "gleicher user");
+                        // gleicher user hat nach login sich wieder eingeloggt
+
+                        // get list of devices
+                        // if last saved device id still exists
+                        // check if tokens match up, if same token
+                        // reselect this device. we are still set up
+
+
+                        // if last saved device id still exists
+                        // check if tokens match up, if different token,
+                        // expect the tolken to have changed
+                        // update the device token and push to server
+                        // a device token change should almost never happen
+
+                        // if the device id no longer exists,
+                        // open device chooser
+                        // save selected device id
+
+                        openDeviceSelection = true;
                     }
 
+                    session.loginUser(sessionResponse.getId(), username, sessionResponse.getName(), sessionResponse.getApiToken());
 
-                    // weil neuer user auch device selection öffnen
-                    // last used device id leeren to not reconnect with wrong
-                    sharedPreferences.edit().putInt(Constants.LAST_SIGNED_IN_DEVICE_ID, 0).commit();
+                    pDialog.dismiss();
 
-                    openDeviceSelection = true;
-
-                } else {
-                    Log.d(TAG, "gleicher user");
-                    // gleicher user hat nach login sich wieder eingeloggt
-
-                    // get list of devices
-                    // if last saved device id still exists
-                    // check if tokens match up, if same token
-                    // reselect this device. we are still set up
-
-
-                    // if last saved device id still exists
-                    // check if tokens match up, if different token,
-                    // expect the tolken to have changed
-                    // update the device token and push to server
-                    // a device token change should almost never happen
-
-                    // if the device id no longer exists,
-                    // open device chooser
-                    // save selected device id
-
-                    openDeviceSelection = true;
-                }
-
-                session.loginUser(sessionResponse.getId(), username, sessionResponse.getName(), sessionResponse.getApiToken());
-
-                pDialog.dismiss();
-
-                if (openDeviceSelection) {
-                    openDeviceSelectionActivity();
-                } else {
-                    loadDashboard();
+                    if (openDeviceSelection) {
+                        openDeviceSelectionActivity();
+                    } else {
+                        loadDashboard();
+                    }
                 }
 
             }
 
             @Override
-            public void onFailure(Call<SessionResponse> call, Throwable t) {
+            public void onFailure(@NonNull Call<SessionResponse> call, @NonNull Throwable t) {
                 Log.d("TAG", "Response = " + t.toString());
                 pDialog.dismiss();
             }
@@ -206,12 +207,12 @@ public class LoginActivity extends AppCompatActivity {
         Call<RemoveTokenResponse> call = apiService.removeToken("Bearer " + sessionResponse.getApiToken(), token);
         call.enqueue(new Callback<RemoveTokenResponse>() {
             @Override
-            public void onResponse(Call<RemoveTokenResponse> call, retrofit2.Response<RemoveTokenResponse> response) {
+            public void onResponse(@NonNull Call<RemoveTokenResponse> call, @NonNull Response<RemoveTokenResponse> response) {
                 Log.d(TAG, "token deleted");
             }
 
             @Override
-            public void onFailure(Call<RemoveTokenResponse> call, Throwable t) {
+            public void onFailure(@NonNull Call<RemoveTokenResponse> call, @NonNull Throwable t) {
                 Log.d("TAG", "Response = " + t.toString());
             }
         });
