e81c9bfa8dccf293d935eaf88a961d76ca230330
Alexander Glück <github@alexanderglueck.at>
2019-11-29 20:43:51 +0100

    Remove volley

--- app/src/main/java/com/alexanderglueck/urlpusher/SelectDeviceActivity.java
+++ app/src/main/java/com/alexanderglueck/urlpusher/SelectDeviceActivity.java
@@ -2,27 +2,13 @@ package com.alexanderglueck.urlpusher;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.AuthFailureError;
-import com.android.volley.DefaultRetryPolicy;
-import com.android.volley.NetworkResponse;
-import com.android.volley.Request;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonArrayRequest;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -33,7 +19,7 @@ public class SelectDeviceActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
 
     private SessionHandler session;
-    List<Device> movieList;
+    List<Device> deviceList;
     RecyclerView recyclerView;
     RecyclerAdapter recyclerAdapter;
 
@@ -45,11 +31,11 @@ public class SelectDeviceActivity extends AppCompatActivity {
 
         session = new SessionHandler(getApplicationContext());
 
-        movieList = new ArrayList<>();
-        recyclerView = (RecyclerView)findViewById(R.id.deviceRecyclerView);
+        deviceList = new ArrayList<>();
+        recyclerView = (RecyclerView) findViewById(R.id.deviceRecyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
-        recyclerAdapter = new RecyclerAdapter(getApplicationContext(),movieList);
+        recyclerAdapter = new RecyclerAdapter(getApplicationContext(), deviceList);
         recyclerView.setAdapter(recyclerAdapter);
 
         ApiInterface apiService = ApiClient.getClient().create(ApiInterface.class);
@@ -57,80 +43,15 @@ public class SelectDeviceActivity extends AppCompatActivity {
         call.enqueue(new Callback<List<Device>>() {
             @Override
             public void onResponse(Call<List<Device>> call, Response<List<Device>> response) {
-                movieList = response.body();
-                Log.d("TAG","Response = "+movieList);
-                recyclerAdapter.setMovieList(movieList);
+                deviceList = response.body();
+                Log.d("TAG", "Response = " + deviceList);
+                recyclerAdapter.setMovieList(deviceList);
             }
 
             @Override
             public void onFailure(Call<List<Device>> call, Throwable t) {
-                Log.d("TAG","Response = "+t.toString());
+                Log.d("TAG", "Response = " + t.toString());
             }
         });
-
-        getDevices();
-    }
-
-    private void getDevices() {
-        final JSONArray request = new JSONArray();
-
-        JsonArrayRequest jsArrayRequest = new JsonArrayRequest
-                (com.android.volley.Request.Method.GET, Constants.URL_FETCH_DEVICES, request, new com.android.volley.Response.Listener<JSONArray>() {
-                    @Override
-                    public void onResponse(JSONArray response) {
-
-                        //Check if user got logged in successfully
-                        Log.d(TAG, response.toString());
-                        Log.d(TAG, "should have deleted token");
-
-                        try {
-                            for (int i = 0; i < response.length(); i++) {
-                                JSONObject jsonData = response.getJSONObject(i);
-                                Log.d(TAG, jsonData.getString("name"));
-                                Log.d(TAG, jsonData.getString("device_token"));
-                                Log.d(TAG, "" + jsonData.getInt("id"));
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }, new com.android.volley.Response.ErrorListener() {
-
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-
-                        Log.d("TSDFEERROR", "" + error.getMessage() + error.getLocalizedMessage() + error.toString());
-
-                        NetworkResponse response = error.networkResponse;
-                        String temp = "";
-                        if (response != null && response.data != null) {
-                            temp = new String(response.data, StandardCharsets.UTF_8);
-                        }
-
-                        Log.d("TSDFEERROR2", "" + temp);
-                        //Display error message whenever an error occurs
-                        Toast.makeText(getApplicationContext(),
-                                error.getMessage(), Toast.LENGTH_SHORT).show();
-
-                    }
-                }) {
-
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("Authorization", "Bearer " + session.getUserDetails().getApiToken());
-                return params;
-            }
-        };
-
-        jsArrayRequest.setRetryPolicy(new DefaultRetryPolicy(
-                15 * 1000,
-                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
-
-        // Access the RequestQueue through your singleton class.
-        MySingleton.getInstance(this).addToRequestQueue(jsArrayRequest);
-
     }
 }
